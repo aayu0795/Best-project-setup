83e982422493e58e05a2902a512b04b66fd41336
aayu0795 <aayushkumar0795@outlook.com>
2020-05-07 02:19:16 +0530

    Cosmetic changes

--- demo/settings/base.py
+++ demo/settings/base.py
@@ -14,7 +14,7 @@ BASE_DIR = os.path.dirname(
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env("SECRET_KEY")
 
 # Application definition
 
